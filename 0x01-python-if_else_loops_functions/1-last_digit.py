e6d5a16cf18738417deb76f9a62102515d54efa3
GodmanOluwaseun <oluwaseuntopeajayi@gmail.com>
2024-03-16 07:06:19 -0700

    Program that assigns random number to a var and checks value of last digit.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,8 +6,8 @@ last_number = abs(number) % 10
 
 if last_number > 5:
     print(f"Last digit of {number} is {last_number} and is greater than 5")
-if last_number == 0:
+elif last_number == 0:
     print(f"Last digit of {number} is {last_number} and is 0")
-if last_number < 6 and last_number != 0:
+elif last_number < 6 and last_number != 0:
     print(f"Last digit of {number} is {last_number} and is less than 6 and\
  not 0")
