19bc840a8bc36df9e6d88343e700008a3cb37a90
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-04-09 14:52:42 -0700

    class Square that defines a square by private attribute /Size/

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+"""
+0-square
+    This module holds an empty class Square
+    that defines a square.
+
+"""
+
+class Square:
+    """Empty class Square that defines a square:"""
+    def __init__(self, size):
+        self.__size  = size
