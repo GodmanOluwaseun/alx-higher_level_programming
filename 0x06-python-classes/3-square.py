068b799b040a20d46d83b3fe5459acb6d341bab1
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-04-09 15:36:57 -0700

    Add method that returns the current square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+0-square
+    This module containss a class Square
+    that defines a square.
+
+Method:
+    area Calculates and returns area of square.
+
+"""
+
+
+class Square:
+    """Class Square that defines a square of size:"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+
+    def area(self):
+        """Area returns current square area"""
+        return self.__size * self.__size
