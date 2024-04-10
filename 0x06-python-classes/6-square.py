b3dcb95b9a3f5257e045ffe993d4e524b6de7d60
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-04-10 07:53:39 -0700

    Class Square that defines a square, and prints by position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -64,7 +64,7 @@ class Square:
             for i in range(self.__size):
                 for j in range(self.__size):
                     if self.__position[1] > 0:
-                        print("_", end="")
+                        print(" ", end="")
                     else:
                         print("#", end="")
                 print()
