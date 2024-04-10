59849904e72b6ddccf74a5238630b3cf9c537002
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-04-10 03:21:28 -0700

    Class Square that defines a square, with method that prints square to stdout with #

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -38,7 +38,7 @@ class Square:
 
     def my_print(self):
         """Prints in stdout square with character #"""
-        for i in range(size):
-            for j in range(size):
+        for i in range(__size):
+            for j in range(__size):
                 print("#", end="")
             print()
