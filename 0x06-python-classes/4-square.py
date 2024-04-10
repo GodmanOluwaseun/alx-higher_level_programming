3b1cd9fd74c746f2c4fc71c766f161bbccf41633
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-04-10 02:55:36 -0700

    Class Square that defines a square, with getter and setter method

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -26,6 +26,10 @@ class Square:
     @size.setter
     def size(self, value):
         """Setter for size attribute"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
