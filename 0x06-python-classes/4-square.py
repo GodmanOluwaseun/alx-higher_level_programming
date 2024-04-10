8e16dbad7723d93e10595322c4da91925f22c2b7
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-04-10 02:51:51 -0700

    Class Square that defines a square, with getter and setter method

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+0-square
+    This module containss a class Square
+    that defines a square.
+
+Method:
+    area Calculates and returns area of square.
+"""
+
+
+class Square:
+    """Class Square that defines a square of size:"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter for size attribute"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for size attribute"""
+        self.__size = value
+
+    def area(self):
+        """Area returns current square area"""
+        return self.__size * self.__size
