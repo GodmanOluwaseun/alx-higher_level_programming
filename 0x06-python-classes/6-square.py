81fea7e02d2bf46bf8ebe27e702c01f3ba7633d5
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-04-10 06:49:31 -0700

    Class Square that defines a square, and prints by position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -18,9 +18,9 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-        if not isinstance(position[0], int) or not \
-        isinstance(position[1], int) or len(position) != 2 \
-        or position[0] < 0 or position[1] <  0:
+        if not isinstance(position[0], int) or \
+                not isinstance(position[1], int) or \
+                len(position) != 2 or position[0] < 0 or position[1] <  0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = position
 
@@ -46,9 +46,9 @@ class Square:
     @position.setter
     def position(self, value):
         """Sets position attribute"""
-        if not isinstance(value[0], int) or not \
-        isinstance(value[1], int) or len(value) != 2 \
-        or value[0] < 0 or value[1] <  0:
+        if not isinstance(value[0], int) or \
+                not isinstance(value[1], int) or \
+                len(value) != 2 or value[0] < 0 or value[1] <  0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
