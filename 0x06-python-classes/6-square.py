154a7ae637845f8b58ac389bfad520ef2c990857
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-04-11 03:37:33 -0700

    Add method that prints the square, with position coordinates

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
-"""6-square.
-
-This module contains a class Square that defines a square.
+"""
+6-square
+    This module contains a class Square that defines a square.
 
 Attributes:
     size (int): Determines length of the square sides, must be +ve int.
