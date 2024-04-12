0fc4efe744e2fe312fbf5a45314fd2fbc394e9ea
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-04-12 10:28:45 -0700

    Empty class Rectangle that defines a rectangle:

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""0-rectangle: Empty class Rectangle that defines a rectangle."""
+
+
+class Rectangle:
+    """Empty rectangle class"""
+    def __init_(self, length=0, breadth=0):
+        self.length = length
+        self.breadth =breadth
