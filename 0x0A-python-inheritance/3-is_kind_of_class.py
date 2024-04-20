1813a35cb3cac585fa33cbca26f37ffda6d01d82
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-04-20 05:33:12 -0700

    Function that checks for superclass

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""3-is_kind_of_class:
+Function that checks if a class inherited from another class.
+
+Return value:
+    returns true or false.
+"""
+
+def is_kind_of_class(obj, a_class):
+    """Checks if obj is a subclass of a_class"""
+    obj_class = type(obj)
+    return issubclass(a_class, obj_class)
