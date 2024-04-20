c309cc99e903c8a81bcfcf75b91a04f1607879c8
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-04-20 04:01:58 -0700

    Function that checks if object is an instance of specified class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""2-is_same_class:
+Function that checks if object is an instance of specified class.
+
+Returns:
+    Returns True
+    otherwise False.
+"""
+
+
+def is_same_class(obj, a_class):
+    """Checks if obj is type a_class"""
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
