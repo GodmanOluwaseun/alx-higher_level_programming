71651d8e0ba20bea19fed45661360c6642f706ac
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-04-21 12:17:02 -0700

    Geometry class, with integer validator method, and test cases.

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""5-base_geometry
+A base geometry class.
+"""
+
+
+class BaseGeometry:
+    """Geometry class with area method"""
+
+    def area(self):
+        """Area method that raises exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Methodd that validates value"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value < 0 or value == 0:
+            raise ValueError("{} must be greater than 0".format(name))
