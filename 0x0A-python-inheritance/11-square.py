dbbc84d475eabbcb85774004cb58f033fcd4dd9a
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-04-22 04:55:02 -0700

    Square class that inherits from Rectangle class, with area and str

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""10-square:
+Class Square that inherits from Rectangle 9-rectangle
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square that inherits Rectangle"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Square area method"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Str method"""
+        width = str(self.__size)
+        height = str(self.__size)
+        string = f"[Rectangle] {width}/{height}"
+        return string
