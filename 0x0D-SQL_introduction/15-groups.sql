6b9069ece56dae5ced50fbc8641d88dabfefcde4
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-05-24 11:49:45 -0700

    Lists count of same scores

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,5 @@
--- List count of recods with same score.
-SELECT COUNT(DISTINCT score), id
+-- List count of records with same score.
+SELECT score, COUNT(score) AS number
 FROM second_table
+GROUP BY score
 ORDER BY score DESC;
