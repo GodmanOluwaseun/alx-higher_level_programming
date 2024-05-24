b078f584916ab772ba8f7dfe0ddc58b6cf5da2bc
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-05-24 10:21:19 -0700

    Creates new table with multiple rows and values

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,5 @@
 -- Creates a new table.
+-- Inserts multiple rows.
 CREATE TABLE IF NOT EXISTS second_table (
 	id INT,
 	name VARCHAR(256),
@@ -7,7 +8,7 @@ CREATE TABLE IF NOT EXISTS second_table (
 
 INSERT INTO second_table (id, name, score,)
 VALUES
-	(1, "John", 10),
-	(2, "Alex", 3),
-	(3, "Bob", 14),
-	(4, "George", 8);
+	(1, 'John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'George', 8);
