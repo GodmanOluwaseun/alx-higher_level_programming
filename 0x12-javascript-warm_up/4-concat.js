f5e0feac59a85ad81b8a959e5fdbc0b782fd518e
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-06-22 15:11:34 -0700

    Script that prints two arguments passed to it, in the following format: “ is ”

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,9 +3,9 @@
 const { argv } = require('node:process');
 
 if (argv.length === 4) {
-  console.log('${argv[3]} is ${argv[4]}');
+  console.log(`${argv[3]} is ${argv[4]}`);
 } else if (argv.length === 3) {
-  console.log('${argv[3]} is undefined');
+  console.log(`${argv[3]} is undefined`);
 } else if (argv.length <= 2) {
-  console.log('undefined is undefined');
+  console.log(`undefined is undefined`);
 }
