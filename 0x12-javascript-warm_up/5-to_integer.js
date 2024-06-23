e574cf6b251efa20257cddccbaaa63faeebcf2b1
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-06-23 13:44:23 -0700

    Script that convert argument to number and prints it

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,12 +2,10 @@
 
 const { argv } = require('node:process');
 
-if (typeof argv[2] === 'number') {
-  console.log(argv[2]);
-} else if (typeof argv[2] === 'string' && !isNaN(Number(argv[2]))) {
-  console.log(Number(argv[2]));
-} else if (typeof argv[2] === 'Bigint') {
-  console.log(Number(argv[2]);
+if (argv.length <= 2) {
+  console.log('Not a number');
+} else if (!isNaN(Number(argv[2]))) {
+  console.log('My number:' argv[2]);
 } else {
   console.log('Not a number');
 }
