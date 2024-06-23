0684b71bc351458920ce59bab1b9f8526abb6afc
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-06-23 15:22:44 -0700

    Script that prints x times “C is fun\”

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const str = 'C is fun';
+const { argv } = require('node:process');
+
+if (argv.length <= 2 || isNaN(Number(argv[2]))) {
+  console.log('Missing number of occurrences');
+} else {
+  const count = Number(argv[2]);
+  if (count < 0) {
+    return;
+  }
+  for (let i = 0; i < count; i++) {
+      console.log(str);
+  }
+}
