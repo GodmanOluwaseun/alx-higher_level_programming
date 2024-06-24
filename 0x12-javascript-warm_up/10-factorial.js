7a7931a79e05785de46df37ccd4086c60e6fcfff
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-06-24 06:14:04 -0700

    Computes and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+function factorial (a) {
+  if (a === 0 || a === 1) {
+    return 1;
+  } else {
+  return a * factorial(a - 1);
+  }
+}
+
+if (argv.length <= 2) {
+  console.log('1');
+  process.exit();
+}
+
+a = Number(argv[2]);
+const fac = factorial(a);
+console.log(fac);
