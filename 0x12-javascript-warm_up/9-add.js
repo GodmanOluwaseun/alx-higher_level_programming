f44d9ac1d2480f2ffee8e98fe4a594fea78b8f39
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-06-24 04:16:52 -0700

    Function that adds 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,13 +3,13 @@
 const { argv } = require('node:process');
 
 if (argv.length <= 3) {
-  console.log('NaN');
+  console.log(NaN);
 }
 
 function add(a, b) {
   a = Number(argv[2]);
   b = Number(argv[3]);
-  let sum = a + b;
+  const sum = a + b;
   console.log(sum);
 }
 
