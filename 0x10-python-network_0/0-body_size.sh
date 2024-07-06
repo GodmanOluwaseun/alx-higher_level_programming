f39b31f29cb5bfa52fd4a6c1fabb5dd551a84982
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-07-06 16:46:47 +0000

    Script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Takes a URL, sends a request, and displays the size of the body of the response in bytes
 
-if [ '$#' -ne 1]; then
-    echo "Usage: $0 url"
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 url"
 fi
 
 content_length=$(curl -sI "$1" | grep -i "Content-Length:" | awk '{print $2}' | tr -d '\r')
