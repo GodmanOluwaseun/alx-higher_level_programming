97183eea4bc5ab400237634f620a845a25c44522
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-07-31 14:26:58 -0700

    Read file and display content

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-const fs = require ('fs');
+const fs = require('fs');
 const arg = process.argv.slice(2);
 const fileName = arg[0];
 
-fs.readFile (fileName, 'utf-8', (err, data) => {
+fs.readFile(fileName, 'utf-8', (err, data) => {
   if (err) throw err;
   else {
     console.log(data);
   }
-})
+});
