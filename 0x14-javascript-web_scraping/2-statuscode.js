d5d1902a5ddb45f092063a74bfded490c996a4a3
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-08-01 09:02:55 -0700

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,7 +4,7 @@ const request = require('request');
 const args = process.argv.slice(2);
 const url = args[0];
 
-request(url, {method: 'GET'}, (err, response, body) => {
+request(url, { method: 'GET' }, (err, response, body) => {
   if (err) {
     console.error('Error:', err);
     process.exit(1);
