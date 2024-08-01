34e43d0af66757cddbb6ad2cb1ee86b7fa834b7c
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-08-01 13:48:58 -0700

    Print no of times a character appears in star wars

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,19 +2,19 @@
 
 const request = require('request');
 const url = process.argv[2];
-const id = "https://swapi-api.alx-tools.com/api/people/18/";
-let count = 0;
+const id = 'https://swapi-api.alx-tools.com/api/people/18/';
+const count = 0;
 
 request(url, (err, response, body) => {
   if (err) {
-     console.error(err);
+    console.error(err);
   } else {
     const data = JSON.parse(body).results;
-    for (let film of data) {
-      for (let character of film.characters) {
-	if (character === id){
-	  count += 1;
-	}
+    for (const film of data) {
+      for (const character of film.characters) {
+        if (character === id) {
+          count += 1;
+        }
       }
     }
     console.log(count);
