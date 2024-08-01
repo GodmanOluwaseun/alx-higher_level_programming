e5652d2d482705fbd979eb4822dbcca557b6e3d2
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-07-31 17:06:46 -0700

    Write to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,8 +3,8 @@
 const fs = require('fs');
 const args = process.argv.slice(2);
 const filePath = args[0];
-let string = args[1];
+const string = args[1];
 
 fs.writeFile(filePath, string, 'utf-8', (err) => {
   if (err) throw err;
-  });
+});
