bec319f4a467d8a9354f99631297c52e9265e4b7
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-08-01 11:42:51 -0700

    Star wars movie title from API

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,7 +4,7 @@ const request = require('request');
 const id = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
 
-request (url, (err, response, body) => {
+request(url, (err, response, body) => {
   if (err) {
     console.error(err);
   } else {
@@ -12,4 +12,3 @@ request (url, (err, response, body) => {
     console.log(data.title);
   }
 });
-
