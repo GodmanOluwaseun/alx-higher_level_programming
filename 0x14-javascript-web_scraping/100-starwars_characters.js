bec8e280c13a3f3ccfa80779e7a57f59bda429b8
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-08-02 09:45:54 -0700

    Prints all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const id = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/api/films';
+const url = 'https://swapi-api.alx-tools.com/api/films/';
 
 request(url + id, (err, response, body) => {
   if (err) {
