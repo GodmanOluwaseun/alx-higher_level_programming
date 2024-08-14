c1ef60ffb49284d3255fb86426a73a15a550d7f8
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-08-14 06:18:59 -0700

    feth value of hello and include in div

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,7 @@
+const url = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
+
+$.getJSON(url, function(data) {
+  $('#hello').text(data.hello);
+}).fail(function() {
+  console.error('Error');
+});
