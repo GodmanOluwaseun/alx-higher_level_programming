5911e189d2acff04a0f83b59904074644116394f
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-08-14 06:40:39 -0700

    Say Hello

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,7 +1,9 @@
-const url = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
+$(document).ready(function() {
+  const url = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
 
-$.getJSON(url, function(data) {
-  $('#hello').text(data.hello);
-}).fail(function() {
-  console.error('Error');
+  $.getJSON(url, function(data) {
+    $('#hello').text(data.hello);
+  }).fail(function() {
+    console.error('Error');
+  });
 });
